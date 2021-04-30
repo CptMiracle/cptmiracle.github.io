94ac411e663d7503d750d336c48dc3cdf03cfa94
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-04-29 20:25:07 -0400

    change name

--- javascript/index.js
+++ javascript/index.js
@@ -3,57 +3,57 @@ function myFunction() {
         document.getElementById("main-card-image-image1").src = this.src='/gifs/jensenmath.gif';
     }
     document.getElementById("main-card-id1").onmouseout = function() {
-        document.getElementById("main-card-image-image1").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image1").src = this.src='/images/StudyPrep home image.jpeg';
     }
 
     document.getElementById("main-card-id2").onmouseover = function() {
         document.getElementById("main-card-image-image2").src = this.src='/gifs/mshavrot.gif';
     }
     document.getElementById("main-card-id2").onmouseout = function() {
-        document.getElementById("main-card-image-image2").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image2").src = this.src='/images/StudyPrep home image.jpeg';
     }
 
     document.getElementById("main-card-id3").onmouseover = function() {
         document.getElementById("main-card-image-image3").src = this.src='/gifs/brianmclogan.gif';
     }
     document.getElementById("main-card-id3").onmouseout = function() {
-        document.getElementById("main-card-image-image3").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image3").src = this.src='/images/StudyPrep home image.jpeg';
     }
     document.getElementById("main-card-id4").onmouseover = function() {
         document.getElementById("main-card-image-image4").src = this.src='/gifs/thephysicsclassroom.gif';
     }
     document.getElementById("main-card-id4").onmouseout = function() {
-        document.getElementById("main-card-image-image4").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image4").src = this.src='/images/StudyPrep home image.jpeg';
     }
     document.getElementById("main-card-id5").onmouseover = function() {
         document.getElementById("main-card-image-image5").src = this.src='/gifs/flippingphysics.gif';
     }
     document.getElementById("main-card-id5").onmouseout = function() {
-        document.getElementById("main-card-image-image5").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image5").src = this.src='/images/StudyPrep home image.jpeg';
     }
     document.getElementById("main-card-id6").onmouseover = function() {
         document.getElementById("main-card-image-image6").src = this.src='/gifs/seanlehan.gif';
     }
     document.getElementById("main-card-id6").onmouseout = function() {
-        document.getElementById("main-card-image-image6").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image6").src = this.src='/images/StudyPrep home image.jpeg';
     }
     document.getElementById("main-card-id7").onmouseover = function() {
         document.getElementById("main-card-image-image7").src = this.src='/gifs/mikedane.gif';
     }
     document.getElementById("main-card-id7").onmouseout = function() {
-        document.getElementById("main-card-image-image7").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image7").src = this.src='/images/StudyPrep home image.jpeg';
     }
     document.getElementById("main-card-id8").onmouseover = function() {
         document.getElementById("main-card-image-image8").src = this.src='/gifs/freecodecamp.gif';
     }
     document.getElementById("main-card-id8").onmouseout = function() {
-        document.getElementById("main-card-image-image8").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image8").src = this.src='/images/StudyPrep home image.jpeg';
     }
     document.getElementById("main-card-id9").onmouseover = function() {
         document.getElementById("main-card-image-image9").src = this.src='/gifs/geeksforgeeks.gif';
     }
     document.getElementById("main-card-id9").onmouseout = function() {
-        document.getElementById("main-card-image-image9").src = this.src='/images/StudyHelper home image.jpeg';
+        document.getElementById("main-card-image-image9").src = this.src='/images/StudyPrep home image.jpeg';
     }
     
   }
@@ -67,7 +67,7 @@ myFunction();
 //             document.getElementById("main-card-image-image"+i).src = this.src='/gifs/jensenmath.gif';
 //         }
 //         document.getElementById("main-card-id"+i).onmouseout = function() {
-//             document.getElementById("main-card-image-image"+i).src = this.src='/images/StudyHelper home image.jpeg';
+//             document.getElementById("main-card-image-image"+i).src = this.src='/images/StudyPrep home image.jpeg';
 //         }
 //     }
 // }
