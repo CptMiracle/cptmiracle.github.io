9cfce236346f51ed9edee7e5a7db6ea925cbb4fe
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-05-05 19:35:42 -0400

    Add click to scroll up

---
+++ javascript/scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#scroll-to-top').fadeIn();
+		} else {
+			$('#scroll-to-top').fadeOut();
+		}
+	});
+});
